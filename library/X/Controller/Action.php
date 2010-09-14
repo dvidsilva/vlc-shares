e869a8a54f0c4b46827e4e0ab4a9f2476210c6a5
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2010-09-14 09:31:53 +0000

    Improvement in debug system Resource init moved to Bootstrap

--- library/X/Controller/Action.php
+++ library/X/Controller/Action.php
@@ -20,25 +20,17 @@ class X_Controller_Action extends Zend_Controller_Action {
 	 */
 	protected $translate = null;
 	
+	/**
+	 * 
+	 * @var Bootstrap
+	 */
+	protected $bootstrap = null;
+	
 	public function init() {
 		
-    	$this->options = new Zend_Config_Ini(X_VlcShares::config());
-    	$this->translate = new Zend_Translate('ini', $this->options->general->get('languageFile', APPLICATION_PATH ."/../languages/en_GB.ini" ));
-    	// INIT DEBUG
-    	if ( $this->options->general->debug_enabled)
-    		X_Env::initDebug($this->options->general->get('debug_path', sys_get_temp_dir().'/vlcShares.debug.log' ));
-			
-		if ( $this->options->general->apache_altPort ) {
-			X_Env::initForcedPort($this->options->general->apache_altPort);
-		}
-    	
-    	X_Env::debug(__METHOD__);
-    	
-    	// INIT TRANSLATOR
-    	X_Env::initTranslator($this->translate );
-    		
-        // INIT PLUGIN SYSTEM
-    	X_Env::initPlugins($this->options->plugins);
+		$this->bootstrap = $this->getFrontController()->getParam('bootstrap');
+    	$this->options = $this->bootstrap->getResource('configs'); 
+    	$this->translate = $this->bootstrap->getResource('translation');
 				
 	}
 	
