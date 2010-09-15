45fe5adc2b67a991d4d4f447e4a8a166a66446d1
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2010-09-15 14:26:15 +0000

    This rev is broken. Don't use it!
    new plugins system api
    moved file system browser to FileSystem plugin

---
+++ trunk/library/X/VlcShares/Plugins/WiimcPlxRenderer.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once ('library/X/VlcShares/Plugins/Abstract.php');
+
+/**
+ * 
+ * @author ximarx
+ *
+ */
+class X_VlcShares_Plugins_WiimcPlxRenderer extends X_VlcShares_Plugins_Abstract {
+
+	
+	
+}
+
+
