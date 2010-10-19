7a869ecd3766244cf6f6568eb4a37c4a63bf0d24
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2010-10-19 09:21:01 +0000

    New transcode profiles for Android Devices based on the one submitted by SilentException New h264/aac profile for Android is the default one. Changed update script

--- scripts/update.sqlite.sql
+++ scripts/update.sqlite.sql
@@ -9,8 +9,7 @@ INSERT INTO configs ( `key`, `value`, `default`, `section`, type, label, descrip
 
 
 -- Update plg_profiles.arg type from varchar255 -> text -- [sqlite don't allow column rename or remove]
-ALTER TABLE plg_profiles RENAME TO plg_profiles_back;
-
+DROP TABLE IF EXISTS plg_profiles;
 DROP INDEX IF EXISTS "plg_profiles_id";
 
 CREATE TABLE plg_profiles (
@@ -25,9 +24,45 @@ CREATE TABLE plg_profiles (
 
 CREATE INDEX "plg_profiles_id" ON "plg_profiles" ("id");
 
-INSERT INTO plg_profiles (id, label, arg, cond_devices, cond_providers, cond_formats, weight) SELECT * FROM plg_profiles_back;
+-- Insert new profiles for 0.5.1
 
-DROP TABLE IF EXISTS plg_profiles_back;
+INSERT INTO plg_profiles (label, arg) VALUES
+	('Hq',
+	'transcode{venc=ffmpeg,vcodec=mp2v,vb=4000,scale=.5,width=640,fps=25,acodec=a52,ab=384,channels=6,samplerate=48000,soverlay}'
+	);	
+	
+INSERT INTO plg_profiles (label, arg) VALUES
+	('Mq',
+	'transcode{venc=ffmpeg,vcodec=mp2v,vb=3000,scale=.5,width=640,fps=25,acodec=a52,ab=384,channels=6,samplerate=48000,soverlay}'
+	);
+	
+INSERT INTO plg_profiles (label, arg) VALUES
+	('Lq',
+	'transcode{venc=ffmpeg,vcodec=mp2v,vb=2000,scale=.5,width=640,fps=25,acodec=a52,ab=384,channels=6,samplerate=48000,soverlay}'
+	);
+	
+INSERT INTO plg_profiles (label, arg, cond_formats) VALUES
+	('AVC/ACC safe profile',
+	'transcode{venc=ffmpeg,vcodec=mp2v,vb=1000,scale=.5,width=640,fps=25,soverlay}',
+	'h264+aac');
+
+INSERT INTO plg_profiles (label, arg, cond_devices, weight) VALUES
+	('Android Phone (HQ)',
+	'transcode{vcodec=h264,venc=x264{no-cabac,level=12,vbv-maxrate=384,vbv-bufsize=1000,keyint=75,ref=3,bframes=0},width=320,height=180,acodec=mp4a,ab=64,vb=384}',
+	1,
+	1);
+
+INSERT INTO plg_profiles (label, arg, cond_devices, cond_formats) VALUES
+	('Android Phone (AVC/AAC)',
+	'transcode{vcodec=h264,venc=x264{no-cabac,level=12,vbv-maxrate=384,vbv-bufsize=1000,keyint=75,ref=3,bframes=0},width=320,height=180,vb=384}',
+	1,
+	'h264+aac');
+	
+	
+INSERT INTO plg_profiles (label, arg, cond_devices) VALUES
+	('Android Phone (LQ)',
+	'transcode{vcodec=mp4v,vb=400,fps=25,scale=0.5,acodec=mp4a,ab=20,channels=2}',
+	1);
 
 -- Update p_outputs.arg type from varchar255 -> text -- [sqlite don't allow column rename or remove]
 ALTER TABLE plg_outputs RENAME TO plg_outputs_back;
@@ -48,4 +83,3 @@ CREATE INDEX "plg_outputs_id" ON "plg_outputs" ("id");
 INSERT INTO plg_outputs (id, label, arg, link, cond_devices, weight) SELECT * FROM plg_outputs_back;
 
 DROP TABLE IF EXISTS plg_outputs_back;
-
