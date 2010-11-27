d3cabb9d0d10d9e1c87d91dd524b52a0dda2bf84
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2010-11-27 15:19:45 +0000

    Added youtube plugin to dev tree in extra Removed backup.sqlite.sql because replaced by backup/restore system

---
+++ trunk/extra/plugins/youtube/application/forms/YoutubeVideo.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once 'X/Env.php';
+
+class Application_Form_YoutubeVideo extends Zend_Form
+{
+    public function init()
+    {
+        // Set the method for the display form to POST
+        $this->setMethod('post');
+ 
+        
+        $decorators = array(
+		    array('ViewHelper'),
+		    array('Errors'),
+		    array('Description', array('tag' => 'p', 'class' => 'description')),
+		);
+        
+        $this->addElement('text', 'idYoutube', array(
+            'label'      => X_Env::_('p_youtube_form_video_label'),
+        	'description'=> X_Env::_('p_youtube_form_video_label_desc'),
+            'required'   => true,
+            'filters'    => array('StringTrim'),
+        	'decorators' => $decorators
+        ));
+
+        $categories = array();
+        
+        foreach (Application_Model_YoutubeCategoriesMapper::i()->fetchAll() as $category) {
+        	/* @var $category Application_Model_YoutubeCategory */
+        	$categories[$category->getId()] = $category->getLabel();
+        }
+        
+        $this->addElement('select', 'idCategory', array(
+            'label'      => X_Env::_('p_youtube_form_video_idCategory'),
+        	'description'=> X_Env::_('p_youtube_form_video_idCategory_desc'),
+			'required'   => true,
+        	'multiOptions' => $categories,
+        	'decorators' => $decorators
+        ));
+        
+        // Add the submit button
+        $this->addElement('submit', 'submit', array(
+            'ignore'   => true,
+        	'required'	=> false,
+            'label'    => X_Env::_('submit'),
+        	'decorators' => array('ViewHelper')
+        ));
+        
+        // Add the submit button
+        $this->addElement('button', 'abort', array(
+            'ignore'   => true,
+            'label'    => X_Env::_('abort'),
+        	'decorators' => array('ViewHelper')
+        ));
+ 
+        // And finally add some CSRF protection
+        $this->addElement('hash', 'csrf', array(
+            //'ignore' => true,
+        	'salt'	=> 'p_youtube_video_new_salt',
+        	'decorators' => array('ViewHelper')
+        ));
+    }
+}
---
+++ trunk/extra/plugins/youtube/public/js/youtube/index.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function() {
+	
+	debug.log('document.ready: /public/js/youtube/index.js');
+	
+	$('.quickbox-add-category').click(function (event) {
+		$('#p_youtube_framecontainer').empty().load(baseUrl + '/youtube/category #p_youtube_categoryform', function () {
+			$('#p_youtube_framecontainer #thumbselect').change(function (event) {
+				var $this = $(this);
+				var value = $(this).val();
+				$('#p_youtube_framecontainer #p_youtube_thumbnailpreview').fadeOut('slow', function () {
+					$(this).find('img').remove();
+					if ( value != '' ) {
+						$(this).append('<img src="' + baseUrl + '/images/youtube/uploads/' + value  + '" />').fadeIn('slow');
+					}
+				});
+				if ( value == 'upload' ) {
+					$('#p_youtube_framecontainer #thumbnail').parent().parent().fadeIn('slow');
+				} else {
+					$('#p_youtube_framecontainer #thumbnail').parent().parent().fadeOut('slow');
+				}
+			});
+		});
+	});
+
+	$('.quickbox-add-account').click(function (event) {
+		$('#p_youtube_framecontainer').empty().load(baseUrl + '/youtube/account #p_youtube_accountform');
+	});
+	
+	$('.quickbox-add-video').click(function (event) {
+		$('#p_youtube_framecontainer').empty().load(baseUrl + '/youtube/video #p_youtube_videoform');
+	});
+	
+	
+	$('.p_youtube_category').click(function (event) {
+		event.preventDefault();
+		event.stopPropagation();
+		$(location).attr('href', $(this).find('a').eq(0).attr('href'));
+	});
+	
+});
