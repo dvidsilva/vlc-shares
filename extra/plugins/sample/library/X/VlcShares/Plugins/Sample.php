c1af510f8f0cdca4e80e645aac89f9fd59aa7644
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2010-12-01 16:47:54 +0000

    Sample plugin for 0.5.1 updated

--- extra/plugins/sample/library/X/VlcShares/Plugins/Sample.php
+++ extra/plugins/sample/library/X/VlcShares/Plugins/Sample.php
@@ -2,41 +2,28 @@
 
 require_once 'X/VlcShares/Plugins/Abstract.php';
 
-
 /**
  * This is a soc plugin:
  * It's an example of how to add warning messages in dashboard
- * This plugin only produce info message for the dashboard
+ * This plugin only produce a warning message for the dashboard
  * @author ximarx
  *
  */
 class X_VlcShares_Plugins_Sample extends X_VlcShares_Plugins_Abstract {
 	
 	public function __construct() {
-		$this->setPriority('getIndexMessages', 1); // i add it near the top of the stack
+		$this->setPriority('getIndexMessages', 1); // i add it near the top of the queue
 	}
 	
 	/**
-	 * Return the message
+	 * Return the HELLO WORLD message
 	 * @param Zend_Controller_Action $this
-	 * @return array The format of the array should be:
-	 * 		array(
-	 * 			array(
-	 * 				'type' => message type,
-	 * 				'text' => the message you want to show,
-	 * 			), ...
-	 * 		)
+	 * @return X_Page_ItemList_Message
 	 */
 	public function getIndexMessages(Zend_Controller_Action $controller) {
-		
-		return array(
-			array(
-				'type' => 'info',
-				'text' => 'Sample plugin installed'
-			),
-		);
-		
+		$message = new X_Page_Item_Message($this->getId(), 'HELLO WORLD');
+		$message->setType(X_Page_Item_Message::TYPE_WARNING);
+		return new X_Page_ItemList_Message(array($message));
 	}
-	
 	
 }
