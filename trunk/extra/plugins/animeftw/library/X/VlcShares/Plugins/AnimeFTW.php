75c791b5adf5f327cfbc4fa745446154b8fdbe3f
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2011-01-24 15:56:46 +0000

    Force animeftw plugin to use proxy features always if enabled

--- trunk/extra/plugins/animeftw/library/X/VlcShares/Plugins/AnimeFTW.php
+++ trunk/extra/plugins/animeftw/library/X/VlcShares/Plugins/AnimeFTW.php
@@ -318,7 +318,7 @@ class X_VlcShares_Plugins_AnimeFTW extends X_VlcShares_Plugins_Abstract implemen
 			} else {
 				// files in videos2.animeftw.tv require a valid referer page to be watched
 				// and animeftw controller does the magic trick
-				if ( strpos($linkUrl, 'videos2.animeftw.tv') !== false && $this->config('proxy.enabled', true) ) {
+				if ( /*strpos($linkUrl, 'videos2.animeftw.tv') !== false && */ $this->config('proxy.enabled', true) ) {
 					
 					// X_Env::routeLink should be deprecated, but now is the best option
 					$linkUrl = X_Env::routeLink('animeftw','proxy', array(
