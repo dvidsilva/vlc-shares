eeda3b617b91956bb1db7d98281b1a453647ec24
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2011-02-14 10:28:28 +0000

    Outputs with new gui

--- trunk/application/controllers/OutputsController.php
+++ trunk/application/controllers/OutputsController.php
@@ -16,9 +16,20 @@ class OutputsController extends X_Controller_Action {
 		
 		$generalOuts = Application_Model_OutputsMapper::i()->fetchAllForDevice(null);
 		
+		$outputs = Application_Model_OutputsMapper::i()->fetchAll();
+		
 		$form = new Application_Form_Output();
 		$form->setAction($this->_helper->url('save', 'outputs'));
 		
+		$devices = array(
+			'' => X_Env::_('p_outputs_devicetype_generic'),
+			X_VlcShares_Plugins_Helper_Devices::DEVICE_WIIMC => 'WiiMC',
+			X_VlcShares_Plugins_Helper_Devices::DEVICE_ANDROID => 'Android',
+			X_VlcShares_Plugins_Helper_Devices::DEVICE_PC => 'Pc',
+		);
+		
+		$this->view->devices = $devices;
+		$this->view->outputs = $outputs;
 		$this->view->form = $form;
 		$this->view->wiimcOuts = $wiimcOuts;
 		$this->view->androidOuts = $androidOuts;
@@ -62,23 +73,98 @@ class OutputsController extends X_Controller_Action {
 	}
 	
 	function removeAction() {
-		$id = $this->getRequest()->getParam('outputId', false);
-		if ( $id !== false ) {
-			$model = new Application_Model_Output();
-			Application_Model_OutputsMapper::i()->find($id, $model);
-			if ( $model->getId() == $id ) {
-				try {
-					Application_Model_OutputsMapper::i()->delete($model);
-					$this->_helper->flashMessenger(X_Env::_('p_outputs_delete_done'));
-				} catch (Exception $e) {
-					$this->_helper->flashMessenger(X_Env::_('p_outputs_err_db'));
-				}
-			} else {
-				$this->_helper->flashMessenger(X_Env::_('p_outputs_err_invaliddata'));
-			}
-		} else {
+		$id = $this->getRequest()->getParam('id', false);
+		$csrf = $this->getRequest()->getParam('csrf', false);
+		
+		if ( $id === false ) {
 			$this->_helper->flashMessenger(X_Env::_('p_outputs_err_invaliddata'));
+			$this->_helper->redirector('index', 'outputs');
 		}
-		$this->_helper->redirector('index', 'outputs');
+		
+		$output = new Application_Model_Output();
+		Application_Model_OutputsMapper::i()->find($id, $output);
+		if ( is_null($output->getId()) ) {
+			$this->_helper->flashMessenger(X_Env::_('p_outputs_err_invaliddata'));
+			$this->_helper->redirector('index', 'outputs');
+		}
+
+		$form = new X_Form();
+		$form->setMethod(Zend_Form::METHOD_POST)->setAction($this->_helper->url('remove', 'outputs', 'default', array('id' => $output->getId())));
+		$form->addElement('hash', 'csrf', array(
+			'salt'  => __CLASS__,
+			'ignore' => true,
+			'required' => false
+		));
+		
+		$form->addElement('hidden', 'id', array(
+			'ignore' => true,
+			'required' => false
+		));
+		
+		$form->addElement('submit', 'submit', array(
+            'ignore'   => true,
+            'label'    => X_Env::_('confirm'),
+        ));
+        
+        $form->addDisplayGroup(array('submit', 'csrf', 'id'), 'buttons', array('decorators' => $form->getDefaultButtonsDisplayGroupDecorators()));
+		
+		// execute delete and redirect to index
+		if ( $form->isValid($this->getRequest()->getPost()) ) {
+			try {
+				Application_Model_OutputsMapper::i()->delete($output);
+				$this->_helper->flashMessenger(X_Env::_('p_outputs_delete_done'));
+			} catch (Exception $e) {
+				$this->_helper->flashMessenger(X_Env::_('p_outputs_err_db'));
+			}
+			$this->_helper->redirector('index', 'outputs');
+		}
+		
+		$form->setDefault('id', $output->getId());
+		
+		$this->view->form = $form;
+		$this->view->output = $output;
+		
 	}
+	
+	function addAction() {
+		
+		$form = new Application_Form_Output();
+		$form->setAction($this->_helper->url('save', 'outputs'));
+		
+		$this->view->form = $form;
+		$this->render('edit');
+	}
+	
+	function editAction() {
+		
+		$id = $this->getRequest()->getParam('id', false);
+		if ( $id === false ) {
+			$this->_helper->flashMessenger(X_Env::_('p_outputs_err_invaliddata'));
+			$this->_helper->redirector('index', 'outputs');
+		}
+		
+		$output = new Application_Model_Output();
+		Application_Model_OutputsMapper::i()->find($id, $output);
+
+		if ( is_null($output->getId()) ) {
+			$this->_helper->flashMessenger(X_Env::_('p_outputs_err_invaliddata'));
+			$this->_helper->redirector('index', 'outputs');
+		}
+		
+		$defaults = array(
+			'id' => $output->getId(),
+			'label' => $output->getLabel(),
+			'link'	=> $output->getLink(),
+			'cond_devices' => $output->getCondDevices(),
+			'arg' => $output->getArg(),
+			'weight' => $output->getWeight(),
+		);
+		
+		$form = new Application_Form_Output();
+		$form->setAction($this->_helper->url('save', 'outputs'));
+		$form->setDefaults($defaults);
+		
+		$this->view->form = $form;
+	}	
+	
 }
