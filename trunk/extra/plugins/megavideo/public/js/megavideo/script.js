c066b698bb02a7736c7deb38099e2d8e4d4c8e6e
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2011-02-16 10:17:57 +0000

    Megavideo: fix to category remove and rename

--- trunk/extra/plugins/megavideo/public/js/megavideo/script.js
+++ trunk/extra/plugins/megavideo/public/js/megavideo/script.js
@@ -21,4 +21,12 @@ $(document).ready(function() {
 		});
 	});
 	*/
+	$('.megavideo-category-rename').click(function () {
+		str_p_megavideo_manage_newcategoryname = typeof(str_p_megavideo_manage_newcategoryname) == 'undefined' ? 'New category name' : str_p_megavideo_manage_newcategoryname;
+		event.preventDefault();
+		var categoryName = prompt(str_p_megavideo_manage_newcategoryname, 'Default');
+		if ( categoryName.trim() != "")
+			window.location = $(this).attr('href') + escape(categoryName.trim());
+	});
+	
 });
