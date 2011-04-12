2a3fc563151a6205fe93a9e14aeda079b9caf856
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2011-04-12 15:30:17 +0000

    MovShare hoster added

--- extra/plugins/movshare/library/X/VlcShares/Plugins/Helper/Hoster/MovShare.php
+++ extra/plugins/movshare/library/X/VlcShares/Plugins/Helper/Hoster/MovShare.php
@@ -1,9 +1,9 @@
 <?php 
 
-class X_VlcShares_Plugins_Helper_Hoster_Vimeo implements X_VlcShares_Plugins_Helper_HostInterface {
+class X_VlcShares_Plugins_Helper_Hoster_MovShare implements X_VlcShares_Plugins_Helper_HostInterface {
 
-	const ID = 'vimeo';
-	const PATTERN = '/http\:\/\/((www\.)?)vimeo\.com\/(?P<ID>[0-9]+)/';
+	const ID = 'movshare';
+	const PATTERN = '/http\:\/\/(www\.)?movshare\.net\/video\/(?P<ID>[a-z0-9]+)(#)?/';
 	
 	private $info_cache = array();
 	
@@ -49,11 +49,8 @@ class X_VlcShares_Plugins_Helper_Hoster_Vimeo implements X_VlcShares_Plugins_Hel
 	 * @return string a playable url
 	 */
 	function getPlayable($url, $isId = true) {
-		if ( !$isId ) {
-			$url = $this->getResourceId($url);
-		}
 		$infos = $this->getPlayableInfos($url, true);
-		return "http://vimeo.com/moogaloop/play/clip:{$url}/{$infos['request_signature']}/{$infos['request_signature_expires']}";
+		return $infos['url'];
 	}
 	
 	/**
@@ -80,31 +77,62 @@ class X_VlcShares_Plugins_Helper_Hoster_Vimeo implements X_VlcShares_Plugins_Hel
 		}
 		
 		// use the api
-		$http = new Zend_Http_Client("http://www.vimeo.com/moogaloop/load/clip:$url/local?param_force_embed=0&param_clip_id=$url&param_show_portrait=0&param_multimoog=&param_server=vimeo.com&param_show_title=0&param_autoplay=0&param_show_byline=0&param_color=00ADEF&param_fullscreen=1&param_md5=0&param_context_id=&context_id=null",
+		$http = new Zend_Http_Client("http://www.movshare.net/video/$url",
 			array(
 				'headers' => array(
-					'User-Agent' => "vlc-shares/".X_VlcShares::VERSION." vimeo/".X_VlcShares_Plugins_Vimeo::VERSION
+					'User-Agent' => "vlc-shares/".X_VlcShares::VERSION." movshare/".X_VlcShares_Plugins_MovShare::VERSION
 				)
 			)
 		);
+		// this allow to store the cookie for multiple requests
+		$http->setCookieJar(true);
 		
 		$datas = $http->request()->getBody();
 		
-		$xml = new SimpleXMLElement($datas);
+		if ( strpos($datas, 'We need you to prove you\'re human') !== false ) {
+			// I have to do the request, again
+			$datas = $http->request()->getBody();
+			if ( strpos($datas, 'We need you to prove you\'re human') !== false ) {
+				throw new Exception("Hoster requires interaction");
+			}
+		}
 		
-		if ( @$xml['error'] ) {
+		// now datas should contains the html
+		// time to grab some informations 
+		
+		if ( strpos($datas, 'This file no longer exists on our servers') !== false ) {
 			throw new Exception("Invalid ID {{$url}}", self::E_ID_INVALID);
 		}
 		
-		$infos = array(
-			'title' => (string) @$xml->video->caption,
-			'description' => '',
-			'length' => X_Env::formatTime((string) @$xml->video->duration),
-			'thumbnail' => (string) @$xml->video->thumbnail,
-			'request_signature' => (string) @$xml->request_signature,
-			'request_signature_expires' => (string) @$xml->request_signature_expires,
-		);
+		$matches = array();
+		if ( !preg_match('/Title\: <\/strong>(?P<title>[^\<]+)</', $datas, $matches)  ) {
+			$title = "";
+		}
+		$title = $matches['title'];
+
+		$matches = array();
+		if ( !preg_match('/Description\: <\/strong>(?P<description>[^\<]+)</', $datas, $matches)  ) {
+			$description = '';
+		}
+		$description = $matches['description'];
 		
+		$length = 0;
+		$thumbnail = '';
+		
+		$matches = array();
+		if ( !preg_match('/param name\=\"src\" value\=\"(?P<video>[^\"]+)\"/', $datas, $matches)  ) {
+			$video = '';
+		}
+		$video = $matches['video'];
+		
+		$infos = array(
+			'title' => $title,
+			'description' => $description,
+			'length' => $length,
+			'thumbnail' => $thumbnail,
+			'url' => $video
+		);
+				
 		// add in cache
 		$this->info_cache[$url] = $infos;
 		
@@ -113,7 +141,7 @@ class X_VlcShares_Plugins_Helper_Hoster_Vimeo implements X_VlcShares_Plugins_Hel
 	}
 	
 	function getHosterUrl($playableId) {
-		return "http://www.vimeo.com/$playableId";
+		return "http://www.movshare.net/video/$playableId";
 	}
 	
 }
