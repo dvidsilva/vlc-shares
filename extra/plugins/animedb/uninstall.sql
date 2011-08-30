16886e9736f56520ed6f062255a7e8f9451841af
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2011-08-30 19:05:16 +0000

    [EXTRA][AnimeDB] New version 0.2.1

--- extra/plugins/animedb/uninstall.sql
+++ extra/plugins/animedb/uninstall.sql
@@ -1,10 +1,2 @@
-DELETE FROM configs WHERE `key` = 'animedb.auth.username';
-DELETE FROM configs WHERE `key` = 'animedb.auth.password';
-DELETE FROM configs WHERE `key` = 'animedb.index.url';
-DELETE FROM configs WHERE `key` = 'animedb.base.url';
-DELETE FROM configs WHERE `key` = 'animedb.login.url';
-DELETE FROM configs WHERE `key` = 'animedb.request.timeout';
-DELETE FROM configs WHERE `key` = 'animedb.request.maxredirects';
-DELETE FROM configs WHERE `key` = 'animedb.hide.useragent';
-
+DELETE FROM configs WHERE `key` LIKE 'animedb.%';
 	
