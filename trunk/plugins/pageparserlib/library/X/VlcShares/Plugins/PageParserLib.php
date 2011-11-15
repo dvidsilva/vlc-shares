f1455814c488e78da32746c7c5118fb70a02b4ff
ximarx@gmail.com <ximarx@gmail.com@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2011-11-15 16:25:18 +0000

    typo fixed

--- trunk/plugins/pageparserlib/library/X/VlcShares/Plugins/PageParserLib.php
+++ trunk/plugins/pageparserlib/library/X/VlcShares/Plugins/PageParserLib.php
@@ -5,4 +5,5 @@ class X_VlcShares_Plugins_PageParserLib extends X_VlcShares_Plugins_Abstract {
     const VERSION = '0.1alpha2';
     const VERSION_CLEAN = '0.1';
 		
+    public function __construct() {   }
 }
