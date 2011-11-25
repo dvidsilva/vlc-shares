17e65a08bb55c2c6fca46fa69d2558cc33b16e1f
ximarx@gmail.com <ximarx@gmail.com@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2011-11-25 15:48:50 +0000

    VideoBB: fix for new link decrypter. New version

--- trunk/plugins/videobb/library/X/VlcShares/Plugins/Helper/Hoster/VideoBB.php
+++ trunk/plugins/videobb/library/X/VlcShares/Plugins/Helper/Hoster/VideoBB.php
@@ -4,9 +4,19 @@ class X_VlcShares_Plugins_Helper_Hoster_VideoBB implements X_VlcShares_Plugins_H
 
 	const ID = 'videobb';
 	const PATTERN = '/http\:\/\/((www\.)?)videobb\.com\/(video\/|watch_video.php\?v\=)(?P<ID>[A-Za-z0-9]+)/';
+
+	const KEY_2 = '226593';
+	
+	const MAGIC_1A = 11;
+	const MAGIC_1B = 77213;
+	const MAGIC_1C = 81371;
+	
+	const MAGIC_2A = 17;
+	const MAGIC_2B = 92717;
+	const MAGIC_2C = 192811;
+	
 	
 	private $info_cache = array();
-	
 	
 	/**
 	 * give the hoster id
@@ -93,12 +103,22 @@ class X_VlcShares_Plugins_Helper_Hoster_VideoBB implements X_VlcShares_Plugins_H
 			throw new Exception("Invalid ID {{$url}}", self::E_ID_INVALID);
 		}
 		
+		// format url
+		
+		$token = @base64_decode(@$json['settings']['config']['token1a']);
+		$cypher = @$json['settings']['video_details']['sece2'];
+		$key1 = @$json['settings']['config']['rkts'];
+		
+		//echo "Cypher: $cypher<br/>Key1: $key1";
+		
+		$passkey = $this->decrypt($cypher, (int) $key1, self::KEY_2);
+		
 		$infos = array(
 			'title' => @$json['settings']['video_details']['video']['title'],
 			'description' => @$json['settings']['video_details']['video']['description'],
 			'length' => 0,
 			'thumbnail' => @$json['settings']['config']['thumbnail'],
-			'url' => @base64_decode(@$json['settings']['config']['token1a'])
+			'url' => $token."&c=".$passkey
 		);
 		
 		// add in cache
@@ -111,5 +131,63 @@ class X_VlcShares_Plugins_Helper_Hoster_VideoBB implements X_VlcShares_Plugins_H
 	function getHosterUrl($playableId) {
 		return "http://www.videobb.com/video/$playableId";
 	}
+	
+	private function decrypt($cypher, $keyOne, $keyTwo) {
+		
+		$C = self::hex2Bin($cypher);
+		
+		$B = array();
+		
+		for ( $i = 0; $i < 384; $i++ ) {
+		
+			$keyOne = ($keyOne * self::MAGIC_1A + self::MAGIC_1B) % self::MAGIC_1C;
+			$keyTwo = ($keyTwo * self::MAGIC_2A + self::MAGIC_2B) % self::MAGIC_2C;
+		
+			$B[$i] = ($keyOne + $keyTwo) % 128;
+		}
+		
+		$x = $y = $z = 0;
+		
+		for ( $i = 255; $i >= 0; $i-- ) {
+			$x = $B[$i];
+			$y = $i % 128;
+			$z = $C[$x];
+			$C[$x] = $C[$y];
+			$C[$y] = $z;
+		}
+		
+		for ( $i = 0; $i < 128; $i++ ) {
+			$C[$i] = $C[$i] ^ $B[$i + 256] & 1;
+		}
+		
+		return self::bin2Hex($C);
+		
+	}
+	
+	private static function hex2Bin($hexString, $trim = true) {
+	
+		$result = '';
+		for ( $i = 0; $i < strlen($hexString); $i++ ) {
+			$result .= str_pad(decbin(hexdec($hexString[$i])), 4, '0', STR_PAD_LEFT);
+		}
+		if ( $trim ) {
+			$result = ltrim($result, '0');
+		}
+		return $result;
+	}
+	
+	private static function bin2Hex($binString) {
+	
+		$result = '';
+		$binString = strrev($binString);
+	
+		for ( $i = 0; $i < strlen($binString); $i = $i + 4) {
+			$segment = substr($binString, $i, 4);
+			$segment = strrev($segment);
+			$segment = str_pad($segment, 4, '0', STR_PAD_LEFT);
+			$result .= dechex(bindec($segment));
+		}
+		return strrev($result);
+	}	
 	
 }
--- trunk/plugins/videobb/library/X/VlcShares/Plugins/VideoBB.php
+++ trunk/plugins/videobb/library/X/VlcShares/Plugins/VideoBB.php
@@ -3,8 +3,8 @@
 
 class X_VlcShares_Plugins_VideoBB extends X_VlcShares_Plugins_Abstract {
 	
-    const VERSION = '0.1.1';
-    const VERSION_CLEAN = '0.1.1';
+    const VERSION = '0.1.2';
+    const VERSION_CLEAN = '0.1.2';
 	
 	function __construct() {
 		
