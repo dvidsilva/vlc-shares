592fed9038c3e5db8421685772d21edc45f0544c
ximarx <ximarx@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2012-01-08 09:14:36 +0000

    Videozer 0.1.3:  - new decrypter

--- trunk/plugins/videozer/library/X/VlcShares/Plugins/Helper/Hoster/Videozer.php
+++ trunk/plugins/videozer/library/X/VlcShares/Plugins/Helper/Hoster/Videozer.php
@@ -7,16 +7,72 @@ class X_VlcShares_Plugins_Helper_Hoster_Videozer implements X_VlcShares_Plugins_
 	
 	private $info_cache = array();
 
-	const KEY_2 = 215678;
 	
-	const MAGIC_1A = 11;
-	const MAGIC_1B = 77213;
-	const MAGIC_1C = 81371;
+	//{{{ MAGICs for BIT
+	const MAGIC_BIT_1A = 11;
+	const MAGIC_BIT_1B = 77213;
+	const MAGIC_BIT_1C = 81371;
 	
-	const MAGIC_2A = 17;
-	const MAGIC_2B = 92717;
-	const MAGIC_2C = 192811;
+	const MAGIC_BIT_2A = 17;
+	const MAGIC_BIT_2B = 92717;
+	const MAGIC_BIT_2C = 192811;
+	//}}}
 	
+
+	//{{{ MAGICs for BIT9300
+	const MAGIC_BIT9300_1A = 26;
+	const MAGIC_BIT9300_1B = 25431;
+	const MAGIC_BIT9300_1C = 56989;
+	
+	const MAGIC_BIT9300_2A = 93;
+	const MAGIC_BIT9300_2B = 32589;
+	const MAGIC_BIT9300_2C = 784152;
+	//}}}
+	
+
+	//{{{ MAGICs for BITBROKEUP
+	const MAGIC_BITBROKEUP_1A = 22;
+	const MAGIC_BITBROKEUP_1B = 66595;
+	const MAGIC_BITBROKEUP_1C = 17447;
+	
+	const MAGIC_BITBROKEUP_2A = 52;
+	const MAGIC_BITBROKEUP_2B = 66852;
+	const MAGIC_BITBROKEUP_2C = 400595;
+	//}}}
+	
+
+	//{{{ MAGICs for BITHEAL
+	const MAGIC_BITHEAL_1A = 10;
+	const MAGIC_BITHEAL_1B = 12254;
+	const MAGIC_BITHEAL_1C = 95369;
+	
+	const MAGIC_BITHEAL_2A = 39;
+	const MAGIC_BITHEAL_2B = 21544;
+	const MAGIC_BITHEAL_2C = 545555;
+	//}}}
+	
+	
+	//{{{ MAGICs for BITLION
+	const MAGIC_BITLION_1A = 82;
+	const MAGIC_BITLION_1B = 84669;
+	const MAGIC_BITLION_1C = 48779;
+	
+	const MAGIC_BITLION_2A = 32;
+	const MAGIC_BITLION_2B = 65598;
+	const MAGIC_BITLION_2C = 115498;
+	//}}}
+	
+	private static $INTERNAL_KEYS = array( 
+		215678,
+		516929,
+		962043,
+		461752,
+		141994
+	);
+	
+	const MAGIC_1 = 950569;
+	
+		
 	/**
 	 * give the hoster id
 	 * @return string
@@ -115,44 +171,92 @@ class X_VlcShares_Plugins_Helper_Hoster_Videozer implements X_VlcShares_Plugins_
 		
 		// format url
 		$token = @base64_decode(@$json['cfg']['environment']['token1']);
+		/*
 		$cypher = @$json['cfg']['info']['sece2'];
 		$key1 = @$json['cfg']['environment']['rkts'];
-		
-		
-		
 		$passkey = $this->decrypt($cypher, (int) $key1, self::KEY_2);
 		
 		//echo "Cypher: $cypher<br/>Key1: $key1<br/>Key2: ".self::KEY_2."<br/>Passkey: $passkey";
 		
 		$link = str_replace(':80', '', $token)."&c=".$passkey;
-
-		/*
-		$ctrlurl = @$json['cfg']['environment']['ctrl_url9'];
-		
-		$datas = new stdClass();
-		$datas->d = $this->getHosterUrl($url);
-		$datas->ut = @$json['cfg']['environment']['ut'];
-		$datas->i = @$json['cfg']['environment']['icode'];
-		$datas->t = time();
-		$datas->ac = "34";
-		$datas->u = "";
-		$datas->c = "b455ce73e469a6f88c581843851cba71";
-		$datas->v = $url;
-		$datas = Zend_Json::encode(array($datas));
-		
-		$http->setUri($ctrlurl)
-			->setMethod(Zend_Http_Client::POST)
-			->setParameterPost('data', $datas)
-			->setParameterPost("checkpromotion", 0);
-		$http->request();
 		*/
+		
+		// prepare token for append: add & as last char if missing
+		$token = rtrim($token, '&').'&';
+		
+		$spen = @$json['cfg']['login']['spen'];
+		$salt = @$json['cfg']['login']['salt'];
+		
+		$algoCtrl = pack('H*', $this->decryptBit($spen, $salt, self::MAGIC_1));
+		$algoCtrl = explode(';', $algoCtrl);
+		
+
+		if ( count($algoCtrl) == 0 || strpos($algoCtrl[0], '&') === false ) {
+			X_Debug::e("Invalid AlgoCtrl: ".print_r($algoCtrl, true));
+			throw new Exception("Invalid AlgoCtrl value", self::E_ID_INVALID);
+		}
+		
+		$keys = array();
+		
+		foreach (explode('&', $algoCtrl[1]) as $val) {
+			list($key, $value) = explode('=', $val);
+			$keys[$key] = $value;
+		}
+		
+		//echo "Keys: ".print_r($keys, true);
+		
+		$key2 = self::$INTERNAL_KEYS[$keys['ik'] - 1];
+		$key1 = @$json['cfg']['environment']['rkts'];
+
+		foreach (explode('&', $algoCtrl[0]) as $val) {
+			list($key, $value) = explode('=', $val);
+
+			$decryptedString = "";
+			$keyString = "";
+			
+			switch ($value) {
+				case 1:
+					$keyString = @$json['cfg']['info']['sece2'];
+					$decryptedString = $this->decryptByte($keyString, $key1, $key2);
+					break;
+				case 2:
+					$keyString = @$json['cfg']['ads']['g_ads']['url'];
+					$decryptedString = $this->decryptBit($keyString, $key1, $key2);
+					break;
+				case 3:
+					$keyString = @$json['cfg']['ads']['g_ads']['type'];
+					$decryptedString = $this->decryptBit9300($keyString, $key1, $key2);
+					break;
+				case 4:
+					$keyString = @$json['cfg']['ads']['g_ads']['time'];
+					$decryptedString = $this->decryptBitLion($keyString, $key1, $key2);
+					break;
+				case 5:
+					$keyString = @$json['cfg']['login']['euno'];
+					$decryptedString = $this->decryptBitHeal($keyString, $key1, $key2);
+					break;
+				case 6:
+					$keyString = @$json['cfg']['login']['sugar'];
+					$decryptedString = $this->decryptBitBrokeUp($keyString, $key1, $key2);
+					break;
+				default:
+					break;
+			}
+			
+			$token .= $key . '=' . $decryptedString . "&";  
+			
+			
+		}
+		
+		$token .= "start=0";
+		
 		
 		$infos = array(
 			'title' => @$json["cfg"]['info']['video']['title'],
 			'description' => @$json["cfg"]['info']['video']['description'],
 			'length' => 0,
 			'thumbnail' => @$json["cfg"]['environment']['thumbnail'],
-			'url' => $link,
+			'url' => $token,
 			'rawinfo' => $json
 		);
 		
@@ -167,36 +271,75 @@ class X_VlcShares_Plugins_Helper_Hoster_Videozer implements X_VlcShares_Plugins_
 		return "http://www.videozer.com/video/$playableId";
 	}
 	
-	private function decrypt($cypher, $keyOne, $keyTwo) {
+	private function decryptBit($arg1, $arg2, $arg3) {
+		return $this->zDecrypt(false, $arg1, $arg2, $arg3, self::MAGIC_BIT_1A, self::MAGIC_BIT_1B, self::MAGIC_BIT_1C, self::MAGIC_BIT_2A, self::MAGIC_BIT_2B, self::MAGIC_BIT_2C);
+	}
+
+	private function decryptBit9300($arg1, $arg2, $arg3) {
+		return $this->zDecrypt(false, $arg1, $arg2, $arg3, self::MAGIC_BIT9300_1A, self::MAGIC_BIT9300_1B, self::MAGIC_BIT9300_1C, self::MAGIC_BIT9300_2A, self::MAGIC_BIT9300_2B, self::MAGIC_BIT9300_2C);
+	}
+
+	private function decryptBitBrokeUp($arg1, $arg2, $arg3) {
+		return $this->zDecrypt(false, $arg1, $arg2, $arg3, self::MAGIC_BITBROKEUP_1A, self::MAGIC_BITBROKEUP_1B, self::MAGIC_BITBROKEUP_1C, self::MAGIC_BITBROKEUP_2A, self::MAGIC_BITBROKEUP_2B, self::MAGIC_BITBROKEUP_2C);
+	}
+
+	private function decryptBitHeal($arg1, $arg2, $arg3) {
+		return $this->zDecrypt(false, $arg1, $arg2, $arg3, self::MAGIC_BITHEAL_1A, self::MAGIC_BITHEAL_1B, self::MAGIC_BITHEAL_1C, self::MAGIC_BITHEAL_2A, self::MAGIC_BITHEAL_2B, self::MAGIC_BITHEAL_2C);
+	}
+
+	private function decryptBitLion($arg1, $arg2, $arg3) {
+		return $this->zDecrypt(false, $arg1, $arg2, $arg3, self::MAGIC_BITLION_1A, self::MAGIC_BITLION_1B, self::MAGIC_BITLION_1C, self::MAGIC_BITLION_2A, self::MAGIC_BITLION_2B, self::MAGIC_BITLION_2C);
+	}
+
+	private function decryptByte($arg1, $arg2, $arg3) {
+		return $this->zDecrypt(true, $arg1, $arg2, $arg3, self::MAGIC_BIT_1A, self::MAGIC_BIT_1B, self::MAGIC_BIT_1C, self::MAGIC_BIT_2A, self::MAGIC_BIT_2B, self::MAGIC_BIT_2C);
+	}
+
 	
-		$C = self::hex2Bin($cypher);
-	
-		$B = array();
-	
-		for ( $i = 0; $i < 384; $i++ ) {
-	
-			$keyOne = ($keyOne * self::MAGIC_1A + self::MAGIC_1B) % self::MAGIC_1C;
-			$keyTwo = ($keyTwo * self::MAGIC_2A + self::MAGIC_2B) % self::MAGIC_2C;
-	
-			$B[$i] = ($keyOne + $keyTwo) % 128;
-		}
-	
+	private function zDecrypt($algo = true, $cipher, $keyOne, $keyTwo, $arg0, $arg1, $arg2, $arg3, $arg4, $arg5) {
+		
 		$x = $y = $z = 0;
-	
-		for ( $i = 255; $i >= 0; $i-- ) {
+		
+		$C = self::hex2Bin($cipher, strlen($cipher) * 4);
+		$len = strlen($C) * 2;
+		if ( $algo ) {
+			$len = 256;
+		}
+		
+		$B = array();
+		$A = array();
+		
+		$i = 0;
+		while ( $i < $len * 1.5 ) {
+			
+			$keyOne = ($keyOne * $arg0 + $arg1) % $arg2;
+			$keyTwo = ($keyTwo * $arg3 + $arg4) % $arg5;
+
+			$B[$i] = ($keyOne + $keyTwo) % ($len / 2);
+			
+			$i++;
+		} 
+		
+		$i = $len;
+		while ( $i >= 0 ) {
+
 			$x = $B[$i];
-			$y = $i % 128;
+			$y = $i % ($len / 2);
 			$z = $C[$x];
 			$C[$x] = $C[$y];
 			$C[$y] = $z;
+			
+			$i--;
 		}
-	
-		for ( $i = 0; $i < 128; $i++ ) {
-			$C[$i] = $C[$i] ^ $B[$i + 256] & 1;
+		
+		$i = 0;
+		while ( $i < $len / 2 ) {
+			$C[$i] = $C[$i] ^ $B[$i + $len] & 1;
+			$i++;
 		}
-	
+		
 		return self::bin2Hex($C);
-	
+		
 	}
 	
 	private static function hex2Bin($hexString, $pad = 256) {
