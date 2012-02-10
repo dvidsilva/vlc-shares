5b51de133d61278a8718bc4208f301862a3967b5
ximarx@gmail.com <ximarx@gmail.com@0dcc2c38-c7ed-29a9-9b5a-bc9144ed271c>
2012-02-10 07:44:40 +0000

    hulu 0.1alpha2

--- trunk/plugins/hulu/library/X/VlcShares/Plugins/Hulu.php
+++ trunk/plugins/hulu/library/X/VlcShares/Plugins/Hulu.php
@@ -3,7 +3,7 @@
 
 class X_VlcShares_Plugins_Hulu extends X_VlcShares_Plugins_Abstract {
 	
-    const VERSION = '0.1alpha';
+    const VERSION = '0.1alpha2';
     const VERSION_CLEAN = '0.1';
 	
 	function __construct() {
